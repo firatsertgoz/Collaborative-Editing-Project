afd79b21b67372d20000c61e994fceaca668215a
Rwemigabo <ericrwemigabo@gmail.com>
2017-09-22 17:20:41 +0200

    Frontend

---
+++ app.js
@@ -0,0 +1,8 @@
+// IIFE keeps our variables private
+// and gets executed immediately!
+(function () {
+  // make doc editable and focus
+  var doc = document.getElementById('doc');
+  doc.contentEditable = true;
+  doc.focus();
+})();
