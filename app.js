4b2673559c638170f0995dfa2b41a6b2d5a7e4df
Firat Sertgoz <firatsertgoz@sabanciuniv.edu>
2017-10-05 17:28:04 +0200

    Backend example

--- app.js
+++
@@ -1,8 +0,0 @@
-// IIFE keeps our variables private
-// and gets executed immediately!
-(function () {
-  // make doc editable and focus
-  var doc = document.getElementById('doc');
-  doc.contentEditable = true;
-  doc.focus();
-})();
